e487aa7c30c0b30b875aec2775875d2b09758090
Marilene Soares da Costa <lene626@gmail.com>
2022-08-31 13:05:59 -0600

    adds sample data to postresql

--- schema.sql
+++ schema.sql
@@ -11,6 +11,34 @@ CREATE TABLE pets(
   id serial NOT NULL,
   name varchar(255) NOT NULL,
   type varchar(255) NOT NULL,
+  zip varchar(255) NOT NULL,
   PRIMARY KEY(id)
 );
+
+
+-- populate db with sample data:
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Mr. Whiskers', 'cat', '98745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Beans', 'cat', '95745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Baguera', 'cat', '88745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Bitey McNibble', 'cat', '78745');
+
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Sam', 'dog', '98745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Bark Twain', 'dog', '68745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Buster', 'dog', '58745');
+
+INSERT INTO profiles(id, name, type, zip)
+VALUES (DEFAULT, 'Chewy', 'dog', '48745');
 
