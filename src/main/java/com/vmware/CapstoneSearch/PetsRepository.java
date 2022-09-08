2fbea8201cc093fef1bd5bbe86ae0940dcc88c5d
merwhite11 <meredith.white91@gmail.com>
2022-09-08 16:06:13 -0700

    end of experimentation day push

--- src/main/java/com/vmware/CapstoneSearch/PetsRepository.java
+++ src/main/java/com/vmware/CapstoneSearch/PetsRepository.java
@@ -1,12 +1,14 @@
 package com.vmware.CapstoneSearch;
 
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
 @Repository
-public interface PetsRepository {
+public interface PetsRepository extends ElasticsearchRepository {
 
 }
 //public interface PetsRepository extends JpaRepository<Pet, Long> {
